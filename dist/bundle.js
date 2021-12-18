367f042211a4c1609fc0e54998e03588623ec114
giotsere <giorgitsereteli@protonmail.com>
2021-12-18 17:02:56 +0200

    started working on css and js dcmntgetbyid

---
+++ dist/bundle.js
@@ -0,0 +1,2 @@
+(()=>{"use strict";document.getElementById("input"),document.getElementById("input-box"),document.getElementById("input-title"),document.getElementById("input-note"),document.getElementById("input-content")})();
+//# sourceMappingURL=bundle.js.map
--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
 import "./scss/style.scss"
+
+const input = document.getElementById("input");
+const inputBox = document.getElementById("input-box");
+const inputTitle = document.getElementById("input-title");
+const inputNote = document.getElementById("input-note");
+const inputContent = document.getElementById("input-content");
+
+// const notesWrapper = document.getElementById("notes-wrapper");
+// const notesWrapper = document.getElementById("note-box");
+// const notesWrapper = document.getElementById("note-title-wrapper");
+// const notesWrapper = document.getElementById("note-title");
+// const notesWrapper = document.getElementById("note-delete-icon");
+// const notesWrapper = document.getElementById("note-content-wrapper");
